6b29ed807c5e99a068ed1d7a067c30a4bef33506
sangwoo <138969571+rejeton1@users.noreply.github.com>
2023-10-04 12:09:08 +0900

    Add files via upload

--- Course_Project_2_Storm.R
+++ Course_Project_2_Storm.R
@@ -5,6 +5,7 @@ library(base)
 library(stats)
 library(dplyr)
 library(stringr)
+library(ggplot2)
 
 ##Data read
 Storm <- read.csv('repdata_data_StormData.csv.bz2')
@@ -31,10 +32,22 @@ group_Storm <- Storm3 %>% group_by(EVTYPE) %>%
   summarize(fatality = sum(FATALITIES), injury = sum(INJURIES))
 
 ##ordering by fatality
-group_Storm[order(group_Storm$fatality, decreasing = TRUE),]
+ordering1 <-
+  group_Storm[order(group_Storm$fatality, decreasing = TRUE),]
 
 ##ordering by injury
-group_Storm[order(group_Storm$injury, decreasing = TRUE),]
+ordering2 <-
+  group_Storm[order(group_Storm$injury, decreasing = TRUE),]
+
+##Plot it
+par(mfrow = c(1,2), cex.axis=0.8)
+barplot(ordering1$fatality[1:3], names.arg = ordering1$EVTYPE[1:3],
+        xlab = "Type", ylab = "Fatality")
+title(main = "Top3 Sum of fatalities by types of event 1950-2011")
+barplot(ordering2$injury[1:3], names.arg = ordering2$EVTYPE[1:3],
+        xlab = "Type", ylab = "Injury")
+
+title(main = "Top3 Sum of Injuries by types of event 1950-2011")
 
 ##Then, the type of event that are most harmful to health is TORNADO
 ###############################
@@ -82,6 +95,13 @@ final_Storm <- filter_Storm %>% group_by(EVTYPE) %>%
   summarize(economic = sum(total))
 
 ##ordering by Damage
-final_Storm[order(final_Storm$economic, decreasing = TRUE),]
+ordering3 <- final_Storm[order(final_Storm$economic, decreasing = TRUE),]
+
+##plot it
+g <- ggplot(ordering3[1:3,], aes(reorder(EVTYPE, economic, decreasing=TRUE), economic/10^9))
+g + geom_bar(stat='identity') + labs(x='Type', y='Economic Damage(Billon dollars)',
+                      title = 'Top3 Type of Event that caused Economic
+                      Damage 1950-2011') + 
+  theme(plot.title = element_text(hjust = 0.5))
 
 ####Then the type of event that make the most Damage is HURRICANE.
